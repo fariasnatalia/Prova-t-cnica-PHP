23d0004cc03ae02ae2ff0892fde172796029dcf6
Natália Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2023-04-02 16:28:09 -0300

    Alteração no código

--- segundoteste/index.php
+++ segundoteste/index.php
@@ -4,18 +4,11 @@ include __DIR__.'/styles.php';
 echo '<h1>Manipulação de Dados com array em PHP: </h1>';
 
 echo '<h2>Fiz uma função em PHP que recebe um array como parâmetro e retorna a média das notas dos alunos.</h2>';
-echo 'A média das notas é: 7.5';
 
 
 
-   $array = [
-    'alunos' => [
-       ['nome' => 'João', 'nota' => 7.5],
-       ['nome' => 'Maria' , 'nota' => 9.0],
-       ['nome' => 'Pedro', 'nota' => 5.5],
-       ['nome' => 'Ana', 'nota' => 8.0]
-     ]
-    ];
+
+  
         function calcularMediaNotas(array $alunos) {
         $soma = 0;
         $numAlunos = count($alunos);
@@ -29,10 +22,16 @@ echo 'A média das notas é: 7.5';
         
         
   }
-  
+    $alunos = [
+    ['nome' => 'João', 'nota' => 7.5],
+    ['nome' => 'Maria' , 'nota' => 9.0],
+    ['nome' => 'Pedro', 'nota' => 5.5],
+    ['nome' => 'Ana', 'nota' => 8.0]
+  ];
+    $resultado = calcularMediaNotas($alunos);
+    
+     echo 'A média das notas é:'.$resultado;
      
     
     
-    
-  
 ?>
