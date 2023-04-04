c801c109e6ea13bbf8efb505baea77ea17434561
Natália Farias Santos <102639784+fariasnatalia@users.noreply.github.com>
2023-04-04 19:04:52 -0300

    Adicionando o CSS

---
+++ styles.php
@@ -0,0 +1,20 @@
+<style>
+html,body{
+    font-family: sans-serif;
+    background-color: #333;
+    color : greenyellow;
+    padding-bottom: 100%;
+}
+
+
+pre{
+    background-color: #444;
+    padding: 10px;
+    font-size: 20px;
+    color: #ecf0f1;
+}
+
+html
+
+
+</style>
